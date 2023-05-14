f02c9c5461eba6c8c5989e548eb4c9ba91aea7e1
Fiskehandleren <carljohanastrup@gmail.com>
2023-05-14 20:04:12 +0200

    Make experience a bit more clear

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -8,6 +8,47 @@ import { Component } from '@angular/core';
 export class ExperienceComponent {
   step = 0;
 
+  experiences = [
+    {
+      company: "Danske Bank",
+      role: "Student Data Scientist",
+      icon: "settings_suggest",
+      tasks: [
+        "Performing customer segmentation using machine learning techniques"
+      ]
+    },
+    {
+      company: "Danske Bank",
+      role: "Student Quant",
+      icon: "account_balance",
+      tasks: [
+        "Implemented crash detection of the nightly batch-jobs calculating valuations and risk for desks, clients and the Danish FSA, resulting in less delays in deliveries",
+        "Simplified risk and value calculations delivered to Market Risk, resulting in faster calculations and a simplified code-base",
+        "Participated in the nightly calculation batch on-call rotation"
+      ]
+    },
+    {
+      company: "Danske Bank",
+      role: "Student Software Engineer",
+      icon: "terminal",
+      tasks: [
+        "Completed an MVP frontend solution in Angular, to be used by case-handlers to process anti-money laundering cases for the whole bank",
+        "Developed customer anti-money laundering questionnaires in C#, deployed a customer frontend in Angular and setup CI/CD for both solutions",
+        "Participated in the nightly calculation batch on-call rotation"
+      ]
+    },
+    {
+      company: "GreenMobility",
+      role: "IT Student",
+      icon: "electric_car",
+      tasks: [
+        "Was the only developer in the company, leading to me being responsible for software development and architecture",
+        "Developed various tools utilised by customer support to reduce the amount of manual labour, and infrastructure to enable customer subscriptions",
+        "Developed data-visualization dashboards for various departments"
+      ]
+    }
+  ];
+
   public setStep(index: number): void {
     this.step = index;
   }
